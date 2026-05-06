1726d3d29b846372ff7b59a1a17e07e1737b3fee
Belrick Stephane <latioms@gmail.com>
2026-05-06 01:11:01 +0100

    refactor: simplify props destructuring in VerifyEmailLayout component

--- apps/web/app/verify-email/layout.tsx
+++ apps/web/app/verify-email/layout.tsx
@@ -1,11 +1,7 @@
-import { ReactNode } from "react";
-
 export const dynamic = "force-dynamic";
 
-export default function VerifyEmailLayout({
-  children,
-}: {
-  children: ReactNode;
-}) {
-  return <>{children}</>;
+export default function VerifyEmailLayout(
+  props: Readonly<{ children: React.ReactNode }>
+) {
+  return <>{props.children}</>;
 }
